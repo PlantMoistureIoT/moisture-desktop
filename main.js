318315801a68230ea6e7d7ac721e1077744501e8
shad0w <shardik99@gmail.com>
2017-01-26 19:03:41 +0530

    Initial commit
    Signed-off-by: shad0w <shardik99@gmail.com>

---
+++ main.js
@@ -0,0 +1,6 @@
+const electron = require('electron')
+const {app, BrowserWindow} = electron
+app.on('ready',() => {
+  let win=new BrowserWindow({width:800, height:600})
+  win.loadURL('file:///home/shad0w/SoilMoistureSensor/index.html')
+})
