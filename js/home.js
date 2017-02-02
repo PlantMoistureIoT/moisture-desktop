1c789acce7ac56d51825123c60a873efbfb8f61e
Madhav Kanbur <abcdjdj@gmail.com>
2017-02-02 21:35:07 +0530

    Dynamically add plant divs
    Signed-off-by: Madhav Kanbur <abcdjdj@gmail.com>

--- js/home.js
+++ js/home.js
@@ -8,6 +8,7 @@ $(document).ready(() => {
     console.log(response);
     });
   }
+  addPlants();
   $(".well").hover(function(){
         $(this).css("background-color", "#4a4a55", "border-color", "#4a4a55");
         }, function(){
@@ -34,4 +35,35 @@ var HttpClient = function() {
         anHttpRequest.open( "GET", aUrl, true );
         anHttpRequest.send( null );
     }
+}
+
+/*
+ * Creates a new plant div and returns it
+ */
+function newPlant(name,id) {
+  var plant = document.createElement('div');
+  plant.className = "col-xs-6 col-lg-4";
+  plant.innerHTML = '<div class="plant col-xs-12 col-lg-12 well" id="' + id + '" >\
+    <div class="row">\
+        <div class="col-xs-2 col-lg-2">\
+           <br />\
+           <i class="fa fa-tint" style="font-size: 5em;"></i>\
+        </div>\
+        <div class="info text-center col-xs-10 col-lg-10" id="info12">\
+           <br />Name: ' + name + '<br />Moisture: 81%<br />Updated: 3 seconds ago<br /><br />\
+        </div>\
+    </div>\
+  </div>';
+
+  return plant;
+}
+
+/*
+ * Test drive the newPlant() function
+ */
+function addPlants() {
+    var plantList = document.getElementById("plantList");
+    plantList.appendChild(newPlant("Banyan",0));
+    plantList.appendChild(newPlant("Pine",1));
+    plantList.appendChild(newPlant("Rosewood",2));
 }
