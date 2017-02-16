d5fa0b3e288573401d6fe0638a573ea31ef5e280
Madhav Kanbur <abcdjdj@gmail.com>
2017-02-16 22:10:12 +0530

    add.js : Check validity of input data
    Signed-off-by: Madhav Kanbur <abcdjdj@gmail.com>

--- js/add.js
+++ js/add.js
@@ -9,7 +9,21 @@ $(document).ready(() => {
       var channel_input = document.getElementById("channel_id");
       var data = {};
       data.name = name_input.value;
+      if(data.name.length==0) {
+          alert("Please enter a valid name!");
+          return;
+      }
       data.channel = channel_input.value;
+      if(data.channel.length==0) {
+          alert("Please enter a valid channel ID!");
+          return;
+      }
+      for(var i = 0 ; i < data.channel.length; ++i) {
+          if(!(data.channel.charAt(i)>='0' && data.channel.charAt(i)<='9')) {
+              alert("Please enter a valid channel ID!");
+              return;
+          }
+      }
       ipcRenderer.send('send-data', data)
       ipcRenderer.send('toggle-add-window')
   })
