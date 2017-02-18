7c24bc6a50325ba1f113a835f25fb85ee540f7e2
Madhav Kanbur <abcdjdj@gmail.com>
2017-02-18 18:21:28 +0530

    add.js : Check existing entries before adding
    Signed-off-by: Madhav Kanbur <abcdjdj@gmail.com>

--- js/add.js
+++ js/add.js
@@ -1,10 +1,27 @@
 const {ipcRenderer} = require('electron')
+const storage = require('electron-json-storage')
+
+var modules = [];
+
+setInterval(readLoop, 500);
+function readLoop() {
+  storage.get('modules.json', function(error, data) {
+    if (error)
+      throw error;
+
+      modules = data;
+      if(modules.length == undefined)
+        modules = [];
+    });
+}
+
 
 $(document).ready(() => {
   $('#close').click(function () {
       ipcRenderer.send('toggle-add-window')
   })
   $('#add').click(function () {
+      console.log(modules);
       var name_input = document.getElementById("name_id");
       var channel_input = document.getElementById("channel_id");
       var data = {};
@@ -21,6 +38,12 @@ $(document).ready(() => {
       for(var i = 0 ; i < data.channel.length; ++i) {
           if(!(data.channel.charAt(i)>='0' && data.channel.charAt(i)<='9')) {
               alert("Please enter a valid channel ID!");
+              return;
+          }
+      }
+      for(var i = 0 ; i < modules.length; ++i){
+          if(data.channel == modules[i].channel) {
+              alert("An entry with this channel already exists!");
               return;
           }
       }
